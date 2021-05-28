f1674482d0531400bb64f907c3b8ae5644e53c8d
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-05-28 15:58:22 +0200

    Added checkbox field for changing the default LaraBug server

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,4 +3,7 @@
         'name' => 'LaraBug',
         'description' => 'Official LaraBug plugin for October CMS',
     ],
+    'fields' => [
+        'use_custom_server' => 'Use custom LaraBug server',
+    ],
 ];
